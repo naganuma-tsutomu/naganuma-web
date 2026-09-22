44fc073911dba4ad603e972df8dcb6243090ce4d
naganuma <m2106m@gmail.com>
2026-09-22 13:23:48 +0900

    style: プロジェクトカードのホバー時に即時赤枠表示と滑らかな沈み込みを両立

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -39,7 +39,7 @@ export default function ProjectCard({ project, index, sample = false }: ProjectC
 
       {/* Main Card Content */}
       <div
-        className={`project-card-surface relative bg-[var(--surface)] overflow-hidden border-2 border-[var(--ink)] h-full flex flex-col transition-transform duration-300 md:translate-x-0 md:translate-y-0 group-hover:translate-x-[3px] group-hover:translate-y-[3px] ${
+        className={`project-card-surface relative bg-[var(--surface)] overflow-hidden border-2 border-[var(--ink)] h-full flex flex-col transition-transform duration-300 md:translate-x-0 md:translate-y-0 group-hover:translate-x-[3px] group-hover:translate-y-[3px] group-hover:border-[var(--red)] group-focus-within:border-[var(--red)] ${
           isFullyInView ? "translate-x-[3px] translate-y-[3px]" : "translate-x-0 translate-y-0"
         }`}
       >
